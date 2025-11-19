5c203578513225830e288e2144d4a3a640566b2d
ansh0014 <anshul.jagota@yahoo.com>
2025-11-19 16:48:50 +0530

    add the sharding router implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,121 @@
-#include <iostream>
-
-#include <ws2tcpip.h>
+// main.cpp
 #include <winsock2.h>
+#include <ws2tcpip.h>
+
+#include <iostream>
 #include <string>
 #include <unordered_map>
 #include <sstream>
 #include <algorithm>
 #include <thread>
 #include <mutex>
+#include <vector>
+#include <cctype>
 
 using namespace std;
 #pragma comment(lib, "Ws2_32.lib")
 
-
 unordered_map<string, string> store;
 mutex storeMutex;
 
-string trim(const string &s) {
+int SLOT_START = 0;
+int SLOT_END = 16383;
+string NODE_ADDR = "127.0.0.1";
+int NODE_PORT = 6379;
+
+
+static const unsigned short crc16tab[256] = {
+    0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50A5, 0x60C6, 0x70E7,
+    0x8108, 0x9129, 0xA14A, 0xB16B, 0xC18C, 0xD1AD, 0xE1CE, 0xF1EF,
+    0x1231, 0x0210, 0x3273, 0x2252, 0x52B5, 0x4294, 0x72F7, 0x62D6,
+    0x9339, 0x8318, 0xB37B, 0xA35A, 0xD3BD, 0xC39C, 0xF3FF, 0xE3DE,
+    0x2462, 0x3443, 0x0420, 0x1401, 0x64E6, 0x74C7, 0x44A4, 0x5485,
+    0xA56A, 0xB54B, 0x8528, 0x9509, 0xE5EE, 0xF5CF, 0xC5AC, 0xD58D,
+    0x3653, 0x2672, 0x1611, 0x0630, 0x76D7, 0x66F6, 0x5695, 0x46B4,
+    0xB75B, 0xA77A, 0x9719, 0x8738, 0xF7DF, 0xE7FE, 0xD79D, 0xC7BC,
+    0x48C4, 0x58E5, 0x6886, 0x78A7, 0x0840, 0x1861, 0x2802, 0x3823,
+    0xC9CC, 0xD9ED, 0xE98E, 0xF9AF, 0x8948, 0x9969, 0xA90A, 0xB92B,
+    0x5AF5, 0x4AD4, 0x7AB7, 0x6A96, 0x1A71, 0x0A50, 0x3A33, 0x2A12,
+    0xDBFD, 0xCBDC, 0xFBBF, 0xEB9E, 0x9B79, 0x8B58, 0xBB3B, 0xAB1A,
+    0x6CA6, 0x7C87, 0x4CE4, 0x5CC5, 0x2C22, 0x3C03, 0x0C60, 0x1C41,
+    0xEDAE, 0xFD8F, 0xCDEC, 0xDDCD, 0xAD2A, 0xBD0B, 0x8D68, 0x9D49,
+    0x7E97, 0x6EB6, 0x5ED5, 0x4EF4, 0x3E13, 0x2E32, 0x1E51, 0x0E70,
+    0xFF9F, 0xEFBE, 0xDFDD, 0xCFFC, 0xBF1B, 0xAF3A, 0x9F59, 0x8F78,
+    0x9188, 0x81A9, 0xB1CA, 0xA1EB, 0xD10C, 0xC12D, 0xF14E, 0xE16F,
+    0x1080, 0x00A1, 0x30C2, 0x20E3, 0x5004, 0x4025, 0x7046, 0x6067,
+    0x83B9, 0x9398, 0xA3FB, 0xB3DA, 0xC33D, 0xD31C, 0xE37F, 0xF35E,
+    0x02B1, 0x1290, 0x22F3, 0x32D2, 0x4235, 0x5214, 0x6277, 0x7256,
+    0xB5EA, 0xA5CB, 0x95A8, 0x8589, 0xF56E, 0xE54F, 0xD52C, 0xC50D,
+    0x34E2, 0x24C3, 0x14A0, 0x0481, 0x7466, 0x6447, 0x5424, 0x4405,
+    0xA7DB, 0xB7FA, 0x8799, 0x97B8, 0xE75F, 0xF77E, 0xC71D, 0xD73C,
+    0x26D3, 0x36F2, 0x0691, 0x16B0, 0x6657, 0x7676, 0x4615, 0x5634,
+    0xD94C, 0xC96D, 0xF90E, 0xE92F, 0x99C8, 0x89E9, 0xB98A, 0xA9AB,
+    0x5844, 0x4865, 0x7806, 0x6827, 0x18C0, 0x08E1, 0x3882, 0x28A3,
+    0xCB7D, 0xDB5C, 0xEB3F, 0xFB1E, 0x8BF9, 0x9BD8, 0xABBB, 0xBB9A,
+    0x4A75, 0x5A54, 0x6A37, 0x7A16, 0x0AF1, 0x1AD0, 0x2AB3, 0x3A92,
+    0xFD2E, 0xED0F, 0xDD6C, 0xCD4D, 0xBDAA, 0xAD8B, 0x9DE8, 0x8DC9,
+    0x7C26, 0x6C07, 0x5C64, 0x4C45, 0x3CA2, 0x2C83, 0x1CE0, 0x0CC1,
+    0xEF1F, 0xFF3E, 0xCF5D, 0xDF7C, 0xAF9B, 0xBFBA, 0x8FD9, 0x9FF8,
+    0x6E17, 0x7E36, 0x4E55, 0x5E74, 0x2E93, 0x3EB2, 0x0ED1, 0x1EF0};
+
+unsigned short crc16(const unsigned char *buf, int len)
+{
+    unsigned short crc = 0;
+    for (int i = 0; i < len; ++i)
+    {
+        crc = (unsigned short)((crc << 8) ^ crc16tab[((crc >> 8) ^ buf[i]) & 0x00FF]);
+    }
+    return crc;
+}
+
+int keySlot(const string &key)
+{
+
+    size_t s = key.find('{');
+    if (s != string::npos)
+    {
+        size_t e = key.find('}', s + 1);
+        if (e != string::npos && e > s + 1)
+        {
+            string sub = key.substr(s + 1, e - (s + 1));
+            unsigned short c = crc16((const unsigned char *)sub.data(), (int)sub.size());
+            return c % 16384;
+        }
+    }
+    unsigned short c = crc16((const unsigned char *)key.data(), (int)key.size());
+    return c % 16384;
+}
+
+string trim(const string &s)
+{
     size_t start = s.find_first_not_of(" \r\n\t");
     size_t end = s.find_last_not_of(" \r\n\t");
-    if (start == string::npos) return "";
+    if (start == string::npos)
+        return "";
     return s.substr(start, end - start + 1);
 }
 
+bool ownsSlot(int slot)
+{
+    return slot >= SLOT_START && slot <= SLOT_END;
+}
 
-void handleClient(SOCKET clientSock, string clientIP) {
-    cout << "Connected: " << clientIP << endl;
-    char buf[1024];
+void sendString(SOCKET sock, const string &s)
+{
+    send(sock, s.c_str(), (int)s.size(), 0);
+}
 
-    while (true) {
-        int bytesReceived = recv(clientSock, buf, sizeof(buf) - 1, 0);
-        if (bytesReceived <= 0) {
-            cout << " Disconnected: " << clientIP << endl;
+void handleClient(SOCKET clientSock, string clientIP)
+{
+    cout << "Connected: " << clientIP << "\n";
+    char buf[4096];
+
+    while (true)
+    {
+        int bytesReceived = recv(clientSock, buf, (int)sizeof(buf) - 1, 0);
+        if (bytesReceived <= 0)
+        {
+            cout << "Disconnected: " << clientIP << "\n";
             closesocket(clientSock);
             return;
         }
@@ -39,67 +123,126 @@ void handleClient(SOCKET clientSock, string clientIP) {
         buf[bytesReceived] = '\0';
         string input(buf);
 
-        
         string clean;
         for (char c : input)
             if (isprint((unsigned char)c) || isspace((unsigned char)c))
                 clean += c;
 
         input = trim(clean);
-        if (input.empty()) continue;
+        if (input.empty())
+            continue;
 
         istringstream iss(input);
-        string command, key, value;
-        iss >> command >> key >> value;
+        string command;
+        iss >> command;
+
+        string key;
+        iss >> key;
+
+        string rest;
+        getline(iss, rest);
+        if (!rest.empty() && rest[0] == ' ')
+            rest.erase(0, 1);
+        string value = trim(rest);
+
         transform(command.begin(), command.end(), command.begin(), ::toupper);
 
         string response;
-        if (command == "SET") {
-            if (key.empty() || value.empty()) {
+        bool hasKey = !key.empty();
+        int slot = -1;
+        if (hasKey)
+            slot = keySlot(key);
+
+        if (hasKey && !ownsSlot(slot))
+        {
+            response = "MOVED " + to_string(slot) + " " + NODE_ADDR + ":" + to_string(NODE_PORT) + "\r\n";
+            sendString(clientSock, response);
+            continue;
+        }
+
+        else if (command == "SET")
+        {
+            if (key.empty() || value.empty())
+            {
                 response = "-ERR wrong number of arguments\r\n";
-            } else {
+            }
+            else
+            {
                 lock_guard<mutex> lock(storeMutex);
                 store[key] = value;
                 response = "+OK\r\n";
-            }
-        } else if (command == "GET") {
-            if (key.empty()) {
-                response = "-ERR wrong number of arguments\r\n";
-            } else {
-                lock_guard<mutex> lock(storeMutex);
-                if (store.find(key) != store.end()) {
-                    const string &val = store[key];
-                    response = "$" + to_string(val.size()) + "\r\n" + val + "\r\n";
-                } else response = "$-1\r\n";
-            }
-        } else if (command == "DEL") {
-            lock_guard<mutex> lock(storeMutex);
-            int deleted = store.erase(key);
-            response = ":" + to_string(deleted) + "\r\n";
-        } else if (command == "QUIT") {
-            response = "+BYE\r\n";
-            send(clientSock, response.c_str(), (int)response.size(), 0);
-            closesocket(clientSock);
-            cout << "[x] Client exited: " << clientIP << endl;
-            return;
-        } else {
-            response = "-ERR unknown command\r\n";
-        }
 
-        send(clientSock, response.c_str(), (int)response.size(), 0);
+
+                string option;
+                iss >> option;
+
+                if (option == "EX")
+                {
+                    int seconds;
+                    iss >> seconds;
+                    if (seconds > 0)
+                    {
+                    
+                        thread([key, seconds]()
+                               {
+                    this_thread::sleep_for(chrono::seconds(seconds));
+                    lock_guard<mutex> lock(storeMutex);
+                    store.erase(key);
+                    cout << "[Node] Expired key: " << key << endl; })
+                            .detach();
+                    }
+                }
+                else if (option == "PX")
+                {
+                    int ms;
+                    iss >> ms;
+                    if (ms > 0)
+                    {
+            
+                        thread([key, ms]()
+                               {
+                    this_thread::sleep_for(chrono::milliseconds(ms));
+                    lock_guard<mutex> lock(storeMutex);
+                    store.erase(key);
+                    cout << "[Node] Expired key: " << key << endl; })
+                            .detach();
+                    }
+                }
+            }
+        }
     }
 }
 
-int main() {
+string getArg(vector<string> &args, const string &name, const string &def)
+{
+    for (size_t i = 0; i + 1 < args.size(); ++i)
+    {
+        if (args[i] == name)
+            return args[i + 1];
+    }
+    return def;
+}
+
+int main(int argc, char *argv[])
+{
+    vector<string> args(argv, argv + argc);
+
+    NODE_PORT = stoi(getArg(args, "--port", "6379"));
+    SLOT_START = stoi(getArg(args, "--slot-start", "0"));
+    SLOT_END = stoi(getArg(args, "--slot-end", "16383"));
+    NODE_ADDR = getArg(args, "--addr", "127.0.0.1");
+
     WSADATA wsa;
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
-        cerr << "WSAStartup failed\n";
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+    {
+        cerr << "WSAStartup failed" << endl;
         return 1;
     }
 
     SOCKET listenSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (listenSock == INVALID_SOCKET) {
-        cerr << "socket() failed\n";
+    if (listenSock == INVALID_SOCKET)
+    {
+        cerr << "socket() failed" << endl;
         WSACleanup();
         return 1;
     }
@@ -107,39 +250,42 @@ int main() {
     sockaddr_in serv{};
     serv.sin_family = AF_INET;
     serv.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv.sin_port = htons(6379);
+    serv.sin_port = htons((unsigned short)NODE_PORT);
 
-    if (bind(listenSock, (sockaddr *)&serv, sizeof(serv)) == SOCKET_ERROR) {
-        cerr << "bind() failed\n";
+    if (bind(listenSock, (sockaddr *)&serv, sizeof(serv)) == SOCKET_ERROR)
+    {
+        cerr << "bind() failed" << endl;
         closesocket(listenSock);
         WSACleanup();
         return 1;
     }
 
-    if (listen(listenSock, SOMAXCONN) == SOCKET_ERROR) {
-        cerr << "listen() failed\n";
+    if (listen(listenSock, SOMAXCONN) == SOCKET_ERROR)
+    {
+        cerr << "listen() failed" << endl;
         closesocket(listenSock);
         WSACleanup();
         return 1;
     }
 
-    cout << "MiniRedis running on port 6379\n";
+    cout << "MiniRedis node running on " << NODE_ADDR << ":" << NODE_PORT
+         << " slots [" << SLOT_START << "-" << SLOT_END << "]\n";
 
-    while (true) {
+    while (true)
+    {
         sockaddr_in client{};
         int clientLen = sizeof(client);
         SOCKET clientSock = accept(listenSock, (sockaddr *)&client, &clientLen);
-        if (clientSock == INVALID_SOCKET) {
-            cerr << "accept() failed\n";
+        if (clientSock == INVALID_SOCKET)
+        {
+            cerr << "accept() failed" << endl;
             continue;
         }
-
         string clientIP = inet_ntoa(client.sin_addr);
-        thread(handleClient, clientSock, clientIP).detach();  // concurrent client handler
+        thread(handleClient, clientSock, clientIP).detach();
     }
 
     closesocket(listenSock);
     WSACleanup();
     return 0;
 }
-
